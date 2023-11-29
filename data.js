0e2ab6074f85d41031a06bfcab9b8b329e1ffc22
Mariano Barella <marianobarella1@gmail.com>
2023-11-29 11:44:19 -0300

    primer commit

---
+++ data.js
@@ -0,0 +1,146 @@
+const ProductsData = [
+    {
+        id: 1,
+        name: 'Xiaomi Redmi Buds Essential',
+        price: 19.899,
+        img: 'img/img-products/xiaomi-redmi-buds-essential.webp',
+        category: 'audio',
+    }
+    ,
+    {
+        id: 2,
+        name: 'Aiwa AW-BT301',
+        price: 29.989,
+        img: 'img/img-products/aiwa-bt301.webp',
+        category: 'audio',
+    }
+    ,
+    {
+        id: 3,
+        name: 'Noblex MNT1050',
+        price: 299.999,
+        img: 'img/img-products/parlante-noblex-mnt-1050.webp',
+        category: 'audio',
+    }
+    ,
+    {
+        id: 4,
+        name: 'Parlante JBL Charge Essential 2',
+        price: 149.989,
+        img: 'img/img-products/parlante-jbl-essential.webp',
+        category: 'audio',
+    }
+    ,
+    {
+        id: 5,
+        name: 'Smart Tv 32¨ Philips Android',
+        price: 125.979,
+        img: 'img/img-products/smart-tv-phillips.webp',
+        category: 'tv',
+    }
+    ,
+    {
+        id: 6,
+        name: 'Smart Tv Samsung',
+        price: 149.999,
+        img: 'img/img-products/smart-tv-samsung.webp',
+        category: 'tv',
+    }
+    ,
+    {
+        id: 7,
+        name: 'Smart TV Quint',
+        price: 120.671,
+        img: 'img/img-products/smart-tv-quint.webp',
+        category: 'tv',
+    }
+    ,
+    {
+        id: 8,
+        name: 'Xbox Series S 512gb',
+        price: 799.999,
+        img: 'img/img-products/xblx.webp',
+        category: 'gaming',
+    }
+    ,
+    {
+        id: 9,
+        name: 'Sony PlayStation 5 825GB',
+        price: 949.999,
+        img: 'img/img-products/sony-ps5.webp',
+        category: 'gaming',
+    }
+    ,
+    {
+        id: 10,
+        name: 'WILSON NFL Super Grip Composite Football',
+        price: 125.999,
+        img: 'img/img-products/wilson-nfl.webp',
+        category: 'sports', 
+    }
+    ,
+    {
+        id: 11,
+        name: 'Bicicleta Nordic X',
+        price: 199.141,
+        img: 'img/img-products/nordic-x.webp',  
+        category: 'sports',
+    }
+    ,
+    {
+        id: 12,
+        name: 'Monopatín de pie Wondrus',
+        price: 19.499,
+        img: 'img/img-products/monopatin-wondrus.webp',
+        category: 'sports',
+    }
+    ,
+    {
+        id: 13,
+        name: 'Buzo Puma Modern Sports',
+        price: 33.666,
+        img: 'img/img-products/buzo-puma.webp',
+        category: 'sports',
+    }
+    ,
+    {
+        id: 14,
+        name: 'H.D.P',
+        price: 16.191,
+        img: 'img/img-products/hdp.webp',   
+        category: 'toys',
+    }
+    ,
+    {
+        id: 15,
+        name: 'Buzz Lightyear Toy Story 4',
+        price: 17.154,
+        img: 'img/img-products/buzz-lightyear.webp',
+        category: 'toys',
+    }
+    ,
+    {
+        id: 16,
+        name: 'Action Figure Thor',
+        price: 4.966,
+        img: 'img/img-products/thor-action-figure.webp',
+        category: 'toys',
+    }
+]
+
+const dividirProductos = (size) => {
+    const productsList = [];
+
+    for (let i = 0; i < ProductsData.length; i += size) {
+        productsList.push(ProductsData.slice(i, i + size));
+    }
+    return productsList;
+}
+
+
+const appState = {
+    products: dividirProductos(6),
+    productLimit: dividirProductos(6).length,
+    currentProductIndex: 0,
+    activeFilter: null,
+}
---
+++ main.js
@@ -0,0 +1,358 @@
+const btnToggle = document.querySelector('.btn-toggle-menu');
+const navMenu = document.querySelector('.nav-menu');
+const header = document.querySelector('header');
+const productsContainer = document.querySelector('.products-container');
+const loadMoreBtn = document.querySelector('.load-more-btn');
+const filterContainer = document.querySelector('.filter-container');
+const categoriesList = document.querySelectorAll('.filter-btn');
+const btnCart = document.querySelector('.btn-cart');
+const cartMenu = document.querySelector('.cart');
+const productsCart = document.querySelector('.cart-content');
+const total = document.querySelector('.total');
+const cartBubbleQuantity = document.querySelector('.cart-quantity');
+const btnClearCart = document.querySelector('.btn-clear-cart');
+const btnBuy = document.querySelector('.btn-buy');
+const modal = document.querySelector('.modal');
+
+// Menú Toggle
+const showMenu = () => {
+    navMenu.classList.toggle('show-nav-menu');
+
+    if (navMenu.classList.contains('show-nav-menu')) {
+        header.style.boxShadow = ('none')
+        btnToggle.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    } else {
+        header.style.boxShadow = ('0 0 0.5rem rgba(0, 0, 0, 0.5)');
+        btnToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    }
+    
+    if (cartMenu.classList.contains('show-cart')) {
+        cartMenu.classList.remove('show-cart');
+        return; 
+    }
+
+}
+
+btnToggle.addEventListener('click', (showMenu));
+
+// Mostrar Carrito 
+
+const showCart = () => {
+    cartMenu.classList.toggle('show-cart');
+
+    if (navMenu.classList.contains('show-nav-menu')) {
+        navMenu.classList.remove('show-nav-menu');
+        return;
+    } 
+}
+
+// Guardar el carrito
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+const guardarCarrito = () => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+const crearPlantillaProducto = (product) => {
+    const {id, name, price, img, category} = product;
+    return `
+    <div class="product">
+        <img class="product-img" src="${img}" alt="">
+        <div class="product-info">
+            <h3 class="product-title">${name}</h3>
+            <p class="product-price">$${price}</p>
+            <p class="product-category">${category.toUpperCase()}</p>
+        </div>
+        <button class="add-to-cart-btn" 
+            data-id="${id}"
+            data-name="${name}"
+            data-price="${price}"
+            data-category="${category}">Add to cart <i class="fa-solid fa-cart-shopping"></i></button>
+    </div>`
+}
+
+const renderizarProductos = (ProductsData) => {
+    productsContainer.innerHTML += ProductsData.map(crearPlantillaProducto).join('');
+}
+
+// Botón cargar más productos
+
+const loadMoreProducts = () => {
+    appState.currentProductIndex += 1;
+    let {products, currentProductIndex} = appState;
+    renderizarProductos(products[currentProductIndex]);
+
+    if(currentProductIndex === products.length - 1) {
+        loadMoreBtn.style.display = 'none';
+    }
+}
+
+// Mostrar visibilidad del botón cargar más
+
+const loadMoreBtnVisibility = () => {
+    if(!appState.activeFilter) {
+        loadMoreBtn.style.display = 'block';
+        return;
+    }
+    loadMoreBtn.style.display = 'none';
+
+}
+
+// Aplicar filtros 
+
+const changebtnActiveState = (selectedCategory) => {
+    const categories = [...categoriesList];
+
+    categories.forEach(filterBtn => {
+        if (filterBtn.dataset.category !== selectedCategory) {
+            filterBtn.classList.remove('active');
+            return
+        } 
+        filterBtn.classList.add('active');
+    })
+}
+
+const cambiarEstadoFiltro = (btn) => {
+    appState.activeFilter = btn.dataset.category;
+    changebtnActiveState(appState.activeFilter);
+    loadMoreBtnVisibility(appState.activeFilter);
+}
+
+const applyFilter = ({target}) => {
+    if (!isInactiveFilterBtn(target)) {
+        return;
+    }
+    cambiarEstadoFiltro(target);
+    productsContainer.innerHTML = '';
+    
+    if (appState.activeFilter) {
+        renderizarProductosFiltrados();
+        appState.currentProductIndex = 0;
+        return; 
+    } 
+    renderizarProductos(appState.products[0]);
+}
+
+const isInactiveFilterBtn = (element) => {
+    return (
+        element.classList.contains('filter-btn') && !element.classList.contains('active')
+    )
+}
+
+
+const renderizarProductosFiltrados = () => {
+    const productosFiltrados = ProductsData.filter((product) => {
+        return product.category === appState.activeFilter
+    })
+    renderizarProductos(productosFiltrados);
+}
+
+// FUNCIONES RELATIVAS AL CARRITO
+
+const createCartProductTemplate = (product) => {
+    console.log(product);
+    const {id, name, price, quantity} = product
+    return `  <div class="cart-item">
+    <div class="item-info">
+      <h3 class="item-title">${name}</h3>
+      <span class="item-price">$${price}</span>
+    </div>
+    <div class="item-handler">
+        <span class="quantity-handler down" data-id=${id}>-</span>
+        <span class="item-quantity">${quantity}</span>
+        <span class="quantity-handler up" data-id=${id}>+</span>
+    </div>
+    </div> `
+}
+
+const renderizarCarrito = () => {
+    if (!cart.length) {
+        productsCart.innerHTML = `<p>Your cart is empty</p>`;
+        return;
+    }
+    productsCart.innerHTML = cart.map(createCartProductTemplate).join('');
+}
+
+/*const renderizarCarrito = () => {
+    if (!cart.length) {
+        productsCart.innerHTML = `<p>'Your cart is empty</p>`;
+        return;
+    }
+    productsCart.innerHTML = cart.map((product) => {
+        return `
+            <div class="cart-product">
+                <img class="cart-product-img" src="${product.img}" alt="">
+                <div class="cart-product-info">
+                    <h3 class="cart-product-title">${product.name}</h3>
+                    <span class="cart-product-price">$${product.price}</span>
+                </div>
+                <button class="cart-product-remove-btn" data-id="${product.id}">Remove</button>`
+    })
+}*/
+
+
+/*const mostrarTotalCarrito = () => {
+    const total = cart.reduce((acc, product) => {
+        return acc + product.price;
+    }, 0);
+    total.textContent = `$${total}`;
+}*/
+const obtenerTotal = () => {
+    return cart.reduce((acc, cur) => {
+        console.log('Price:', cur.price);
+        console.log('Quantity:', cur.quantity);
+        return acc + Number(cur.price) * cur.quantity;
+    }, 0);
+}
+
+const mostrarTotalCarrito = () => {
+    total.innerHTML = `$${obtenerTotal().toFixed(2)}`
+}
+
+
+const renderizarBubble = () => {
+    cartBubbleQuantity.innerHTML = cart.length;
+}
+
+// Desabilitar botones
+
+const desabilitarBtn = (btn) => {
+    if (!cart.length) {
+        btn.classList.add('disabled-btn');
+        return;
+    }
+    btn.classList.remove('disabled-btn');
+}
+
+const actualizarCarrito = () => {
+    guardarCarrito();
+    renderizarCarrito();
+    mostrarTotalCarrito();
+    renderizarBubble();
+    desabilitarBtn(btnClearCart);
+    desabilitarBtn(btnBuy);
+}
+
+const anadirProducto = (e) => {
+    if (!e.target.classList.contains('add-to-cart-btn')) {
+        return;
+    }
+    const product = crearProductInfo(e.target.dataset);
+    if(checkExistingProduct(product)) {
+        añadirUnidad(product);
+        mostrarModal(`A unit of ${product.name} has been added to cart`);
+    } else {
+        crearProductoCarrito(product);
+        mostrarModal(`Added ${product.name} to cart`);
+    }
+    actualizarCarrito();
+}
+
+
+const mostrarModal = (mensaje) => {
+    modal.classList.toggle('show-modal');
+    modal.textContent = mensaje;
+    setTimeout(() => {
+        modal.classList.remove('show-modal');
+    }, 1500);
+    //console.log('modal', modal);
+}
+
+const añadirUnidad = (product) => {
+    cart = cart.map((cartProduct) => {
+        return cartProduct?.id === product.id ? {...cartProduct, quantity: cartProduct.quantity + 1} : cartProduct;
+    })
+}
+
+const crearProductoCarrito = (product) => {
+    cart = [...cart, {...product, quantity: 1}];
+}
+
+const checkExistingProduct = (product) => {
+    return cart.find((item) => {
+        return item.id === product.id;
+    })
+}
+
+const crearProductInfo = (product) => {
+    const {id, name, price} = product;
+    return {id, name, price};
+}
+
+// Manejar cantidades 
+
+const manejarCantidades = (e) => {
+    if(e.target.classList.contains('down')) {
+        btnDownEvent(e.target.dataset.id);
+    } else if (e.target.classList.contains('up')) {
+        btnUpEvent(e.target.dataset.id);
+    }
+    actualizarCarrito();
+}
+btnUpEvent = (id) => {
+    const existingProduct = cart.find((product) => product.id === id);
+    añadirUnidad(existingProduct);
+    actualizarCarrito();    
+}
+
+const btnDownEvent = (id) => {
+    const existingProduct = cart.find((product) => product.id === id);
+    if(existingProduct.quantity === 1) {
+        removerProductoCarrito(existingProduct);
+        return;
+    } 
+    sustraerUnidad(existingProduct);
+}
+
+
+const sustraerUnidad = (existingProduct) => {
+    cart = cart.map((product) => {
+        return product.id === existingProduct.id ? {...product, quantity: product.quantity - 1} : product;
+    })
+}
+
+const removerProductoCarrito = (existingProduct) => {
+    cart = cart.filter((product) => {
+        return product.id !== existingProduct.id;
+    })
+    actualizarCarrito();
+}
+
+// Confirmar compra
+
+const completarAccion = (confirmar, exito) => {
+    if(!cart.length) return;
+    if(window.confirm(confirmar)) {
+        cart = [];
+        actualizarCarrito();
+        alert(exito);
+    }
+}
+
+const completarCompra = () => {
+    completarAccion('Are you sure you want to buy these products?', 'Thank you for your purchase!');        
+}
+
+const vaciarCarrito = () => {
+    completarAccion('Are you sure you want to clear the cart?', 'Cart cleared!');
+}
+
+// Función Init
+const init = () => {
+    renderizarProductos(appState.products[0]);
+    loadMoreBtn.addEventListener('click', loadMoreProducts);
+    filterContainer.addEventListener('click', applyFilter);
+    btnCart.addEventListener('click', (showCart));
+    productsContainer.addEventListener('click', anadirProducto);
+    document.addEventListener('DOMContentLoaded', renderizarCarrito);
+    document.addEventListener('DOMContentLoaded', mostrarTotalCarrito);
+    renderizarBubble();
+    desabilitarBtn(btnClearCart);
+    desabilitarBtn(btnBuy);
+    productsCart.addEventListener('click', manejarCantidades);
+    btnBuy.addEventListener('click', completarCompra)
+    btnClearCart.addEventListener('click', vaciarCarrito);
+
+};
+
+init();
